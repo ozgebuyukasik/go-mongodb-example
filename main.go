c0ff644617ea8a4ca5f3a86a5d11f414d544079f
ozgeb <ozge.bykask@outlook.com>
2022-03-22 00:27:51 +0300

    add http router mechanism

--- main.go
+++ main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2"
+	"net/http"
+)
+
+func main() {
+	router := httprouter.New()
+	userController := controllers.NewUserController(getSession())
+	router.GET("/user/:id", userController.GetUser)
+	router.POST("/user", userController.CreateUser)
+	router.DELETE("/user/:id", userController.DeleteUser)
+
+	http.ListenAndServe("localhost:9000", router)
+}
+
+func getSession() *mgo.Session {
+	session, error := mgo.Dial("mongodb://localhost:27107")
+
+	if error != nil {
+		panic(error)
+	}
+	return session
+}
